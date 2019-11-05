9695bf56654bbdfb110b6e2d6b10b9429b641b34
Anna Ponomareva <aponomareva@luxoft.com>
2019-11-05 13:11:58 +0300

    правка database

--- schema.sql
+++ schema.sql
@@ -1,50 +1,45 @@
-CREATE DATABASE yeticave; -- это в консоль?
+-- drop database if exist
+-- кодировка
+CREATE DATABASE yeticave;
+
+USE yeticave;
 
 CREATE TABLE categories (
-    id AUTO_INCREMENT PRIMARY KEY,
-    name CHAR(128),
-    symbol_code CHAR(128),
-);
-
-CREATE TABLE lots (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    name CHAR(128) NOT NULL,
-    description CHAR(255) NOT NULL,
-    img TEXT,
-    start_price INT,
-    end_date INT,
-    step INT,
-    author_id CHAR(128) NOT NULL,
-    winner_id CHAR(128) NOT NULL,
-    category_id INT NOT NULL,
-
-    FOREIGN KEY (author_id) REFERENCES users (id),
-    FOREIGN KEY (winner_id) REFERENCES users (id), -- на какую таблицу ссылается?
-    FOREIGN KEY (category_id) REFERENCES categories (id),
+    id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    name VARCHAR(128) UNIQUE,
+    symbol_code CHAR(128)
 );
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    email CHAR(128) NOT NULL,
-    name CHAR(128) NOT NULL,
-    password CHAR(128) NOT NULL,
-    contact VARCHAR,
-    lot_id INT, -- их же может быть много, можно называть во множественном числе?
-    bet_id INT,
+    register_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    email VARCHAR(128) NOT NULL UNIQUE,
+    name VARCHAR(128) NOT NULL,
+    password VARCHAR(128) NOT NULL UNIQUE,
+    contact VARCHAR(128)
+);
 
-    FOREIGN KEY (lot_id) REFERENCES lots (id),
-    FOREIGN KEY (bet_id) REFERENCES bets (id),
+CREATE TABLE lots (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    create_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    description TEXT NOT NULL,
+    img TEXT NOT NULL,
+    start_price INT NOT NULL,
+    end_date DATETIME NOT NULL,
+    step INT NOT NULL,
+    author_id INT NOT NULL,
+    winner_id INT,
+    category_id INT NOT NULL
 );
 
 CREATE TABLE bets (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+    date DATETIME DEFAULT CURRENT_TIMESTAMP,
     price INT NOT NULL,
-    user_id INT,
-    lot_id INT,
-
-    FOREIGN KEY (lot_id) REFERENCES lots (id),
-    FOREIGN KEY (user_id) REFERENCES users (id),
+    user_id INT NOT NULL,
+	lot_id INT
 );
+
+-- прописать связи отдельно
+-- связи в php
