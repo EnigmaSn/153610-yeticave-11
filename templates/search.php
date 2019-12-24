442b1b0170dff0e0f60fa2bd56e5c4aa9dc732ca
Anna Ponomareva <aponomareva@luxoft.com>
2019-12-24 09:47:23 +0300

    Исправление ошибки при пустом поиске

--- templates/search.php
+++ templates/search.php
@@ -41,15 +41,7 @@
             <h3><?= $error; ?></h3>
         <?php endif; ?>
     </section>
-<!--    <ul class="pagination-list">-->
-<!--        <li class="pagination-item pagination-item-prev"><a>Назад</a></li>-->
-<!--        <li class="pagination-item pagination-item-active"><a>1</a></li>-->
-<!--        <li class="pagination-item"><a href="#">2</a></li>-->
-<!--        <li class="pagination-item"><a href="#">3</a></li>-->
-<!--        <li class="pagination-item"><a href="#">4</a></li>-->
-<!--        <li class="pagination-item pagination-item-next"><a href="#">Вперед</a></li>-->
-<!--    </ul>-->
-    <?php if (count($pages) > 1) {
+    <?php if (count((array) $pages) > 1) {
         echo include_template('paginator.php', [
             'pages'    => $pages,
             'cur_page' => $cur_page,
