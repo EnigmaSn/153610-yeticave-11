bf4b7e632e9dde874de526e92fefe1c528e3db8c
Anna Ponomareva <aponomareva@luxoft.com>
2019-12-24 10:25:43 +0300

    isset login.php

--- templates/login.php
+++ templates/login.php
@@ -7,12 +7,16 @@
     <div class="form__item <?php if(isset($errors['email'])): ?>form__item--invalid<?php endif; ?>"> <!-- form__item--invalid -->
         <label for="email">E-mail <sup>*</sup></label>
         <input id="email" type="text" name="email" placeholder="Введите e-mail" value="<?= get_post_val('email') ?>">
-        <span class="form__error"><?= esc($errors['email']); ?></span>
+        <span class="form__error">
+            <?= (isset($errors['email']) ? esc($errors['email']) : null); ?>
+        </span>
     </div>
     <div class="form__item form__item--last <?php if(isset($errors['password'])): ?>form__item--invalid<?php endif; ?>">
         <label for="password">Пароль <sup>*</sup></label>
         <input id="password" type="password" name="password" placeholder="Введите пароль">
-        <span class="form__error"><?= esc($errors['password']); ?></span>
+        <span class="form__error">
+            <?= (isset($errors['password']) ? esc($errors['password']) : null); ?>
+        </span>
     </div>
     <button type="submit" class="button">Войти</button>
 </form>
